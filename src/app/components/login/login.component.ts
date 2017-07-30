c34067183da5384cb758b87a46b2cbf8fbeff91c
colvxueying <791009808@qq.com>
2017-07-30 22:13:57 +0800

    导航ui

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
-import { TabsetComponent } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-login',
